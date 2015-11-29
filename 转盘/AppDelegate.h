7b629b5fd5b2b747b60cdf0942eb74e858c8466b
Jusive <1345266022@qq.com>
2015-11-29 14:52:05 +0800

    Initial Commit

---
+++ 转盘/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  转盘
+//
+//  Created by Jusive on 15/11/29.
+//  Copyright © 2015年 Jusive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 转盘/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  转盘
+//
+//  Created by Jusive on 15/11/29.
+//  Copyright © 2015年 Jusive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
